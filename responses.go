80d138322b5d4410406acbc16c9fe34d1723d26e
Mike <mikelpsv@gmail.com>
2022-08-29 10:49:25 +0300

    middleware

--- responses.go
+++ responses.go
@@ -27,3 +27,15 @@ func ResponseERROR(w http.ResponseWriter, statusCode int, err error) {
 	ResponseJSON(w, http.StatusBadRequest, nil)
 	return
 }
+
+func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.Header().Set("Vary", "Accept-Encoding")
+		next(w, r)
+	}
+}
