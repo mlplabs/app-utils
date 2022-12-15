9eade000a1a36c20c7846becf785555cc2f7e432
Mike <mikelpsv@gmail.com>
2022-12-15 13:35:13 +0300

    +shedule

--- utils.go
+++ utils.go
@@ -1,1 +1,19 @@
 package utils
+
+import "time"
+
+// Schedule запускает выполнение заданный процедуры с заданным интервалом времени
+func Schedule(fn func(), interval time.Duration, done <-chan bool) *time.Ticker {
+	ticker := time.NewTicker(interval)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				fn()
+			case <-done:
+				return
+			}
+		}
+	}()
+	return ticker
+}
